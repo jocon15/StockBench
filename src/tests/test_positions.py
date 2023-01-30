3c35b968364e20e269ae4d2ce772be4c9eb4b086
Jason O <jaywoc12@gmail.com>
2023-01-29 23:58:40 -0500

    added testing files

--- src/tests/test_positions.py
+++ src/tests/test_positions.py
@@ -23,3 +23,43 @@ def test_close_position():
     # normal close
     position.close_position(200.0)
 
+
+def test_profit_loss():
+    # create a position
+    position = Position(150.0, 2)
+    # ensure accurate profit/loss calculation
+    assert (position.profit_loss(200.0) - 100.0) < MACHINE_EPSILON
+
+
+def test_lifetime_profit_loss():
+    # create a position
+    position = Position(150.0, 2)
+    # liquidate the position
+    position.close_position(200.0)
+    # ensure accurate profit/loss calculation
+    assert (position.lifetime_profit_loss() - 100.0) < MACHINE_EPSILON
+
+
+def test_get_buy_price():
+    # create a position
+    position = Position(150.0, 2)
+    # ensure accuracy of buy price
+    assert (position.get_buy_price() - 150.0) < MACHINE_EPSILON
+
+
+def test_get_share_count():
+    # create a position
+    position = Position(150.0, 2)
+    # ensure accuracy of the share count
+    assert (position.get_share_count() - 2.0) < MACHINE_EPSILON
+
+
+def test_get_sell_price():
+    # create a position
+    position = Position(150.0, 2)
+    # ensure the initial sell price is None
+    assert position.get_sell_price() is None
+    # liquidate the position
+    position.close_position(200.0)
+    # ensure the accuracy of the sell price
+    assert (position.get_sell_price() - 200.0) < MACHINE_EPSILON

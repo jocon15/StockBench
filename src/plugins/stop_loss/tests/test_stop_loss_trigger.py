6f00f325324dded1c388ec1e7daad8d4c51b4558
dev sys <jaywoc12@gmail.com>
2023-10-02 23:57:27 -0400

    created rest of test folders for all plugins

---
+++ src/plugins/stop_loss/tests/test_stop_loss_trigger.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+# allows import out a directory
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+
+
+from plugins.stop_loss.trigger import StopLossTrigger
+
+
+# create test object
+test_obj = StopLossTrigger('stop_loss')
+
+
+def test_additional_days():
+    assert test_obj.additional_days('') == 0

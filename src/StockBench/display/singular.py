5090238fda0d484d58ae4e3926c1a445622207b6
dev sys <jaywoc12@gmail.com>
2023-12-06 00:40:15 -0500

    removed white border from chart

--- src/StockBench/display/singular.py
+++ src/StockBench/display/singular.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import plotly.offline as offline
 from plotly.subplots import make_subplots
 from StockBench.function_tools.nonce import datetime_nonce
 from StockBench.plugin.plugin_interface import PluginInterface
@@ -122,11 +123,31 @@ class SingularDisplay:
         # make the directories if they don't already exist
         os.makedirs(os.path.dirname(chart_filepath), exist_ok=True)
 
+        config = dict({
+            'scrollZoom': False,
+            'displayModeBar': False,
+            'editable': False
+        })
+
+        plot_div = offline.plot(fig, config=config, output_type='div')
+
+        new_fig = """
+                    <head>
+                    <body style="background-color:#202124;">
+                    </head>
+                    <body>
+                    {plot_div:s}
+                    </body>""".format(plot_div=plot_div)
+
         if show and not save:
             fig.show()
         if save and not show:
             fig.write_html(chart_filepath, auto_open=False)
+            with open(chart_filepath, 'w', encoding="utf-8") as file:
+                file.write(new_fig)
         if show and save:
-            fig.write_html(chart_filepath, auto_open=True)
+            with open(chart_filepath, 'w', encoding="utf-8") as file:
+                file.write(new_fig)
+            os.startfile(chart_filepath)
 
         return chart_filepath

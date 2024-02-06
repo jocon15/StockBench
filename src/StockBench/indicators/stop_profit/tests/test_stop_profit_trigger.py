7ceb53381f2678ec43b701772c84b7f91ee29554
dev sys <jaywoc12@gmail.com>
2024-02-05 21:43:39 -0500

    implemented tests for stop profit and stop loss

--- src/StockBench/indicators/stop_profit/tests/test_stop_profit_trigger.py
+++ src/StockBench/indicators/stop_profit/tests/test_stop_profit_trigger.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import pytest
+from unittest.mock import patch
 
 # allows import out a directory
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
@@ -8,9 +10,143 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 from StockBench.indicators.stop_profit.trigger import StopProfitTrigger
 
 
-# create test object
-test_obj = StopProfitTrigger('stop_profit')
+@pytest.fixture
+def test_object():
+    return StopProfitTrigger('stop_loss')
 
 
-def test_additional_days():
-    assert test_obj.additional_days('', '') == 0
+def test_additional_days(test_object):
+    # ============= Arrange ==============
+
+    # ============= Act ==================
+    actual = test_object.additional_days('', '')
+
+    # ============= Assert ===============
+    assert type(actual) is int
+    assert actual == 0
+
+
+def test_add_to_data(test_object):
+    # ============= Arrange ==============
+
+    # ============= Act ==================
+
+    # ============= Assert ===============
+    try:
+        test_object.add_to_data('', '', '', None)
+        assert True
+    except Exception as e:
+        print(e)
+        assert False
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_intraday_percent_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.intraday_profit_loss_percent.return_value = -1.5
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss_intraday', '1%', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is True
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_intraday_percent_not_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.intraday_profit_loss_percent.return_value = -0.5
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss_intraday', '1%', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is False
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_intraday_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.intraday_profit_loss.return_value = -1500
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss_intraday', '1000', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is True
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_intraday_not_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.intraday_profit_loss.return_value = -150
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss_intraday', '1000', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is False
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_lifetime_percent_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.profit_loss_percent.return_value = -1.1
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss', '1%', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is True
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_lifetime_percent_not_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.profit_loss_percent.return_value = -0.1
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss', '1%', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is False
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_lifetime_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.profit_loss.return_value = -1500
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss', '1000', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is True
+
+
+@patch('StockBench.position.position.Position')
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_lifetime_not_hit_case(data_mocker, position_mocker, test_object):
+    # ============= Arrange ==============
+    position_mocker.profit_loss.return_value = -100
+
+    # ============= Act ==================
+    actual = test_object.check_trigger('stop_loss', '1000', data_mocker, position_mocker, 0)
+
+    # ============= Assert ===============
+    assert position_mocker.profit_loss_percent.called is True
+    assert actual is False

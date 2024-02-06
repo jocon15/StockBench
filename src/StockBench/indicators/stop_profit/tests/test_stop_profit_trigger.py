bbef3d7cfaae1f38e3aa8df548af139c80d7d47b
dev sys <jaywoc12@gmail.com>
2024-02-05 21:48:01 -0500

    renamed loss to profit in stop profit tests

--- src/StockBench/indicators/stop_profit/tests/test_stop_profit_trigger.py
+++ src/StockBench/indicators/stop_profit/tests/test_stop_profit_trigger.py
@@ -12,7 +12,7 @@ from StockBench.indicators.stop_profit.trigger import StopProfitTrigger
 
 @pytest.fixture
 def test_object():
-    return StopProfitTrigger('stop_loss')
+    return StopProfitTrigger('stop_profit')
 
 
 def test_additional_days(test_object):
@@ -44,13 +44,13 @@ def test_add_to_data(test_object):
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_intraday_percent_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.intraday_profit_loss_percent.return_value = -1.5
+    position_mocker.intraday_profit_profit_percent.return_value = -1.5
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss_intraday', '1%', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit_intraday', '1%', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is True
 
 
@@ -58,13 +58,13 @@ def test_check_trigger_intraday_percent_hit_case(data_mocker, position_mocker, t
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_intraday_percent_not_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.intraday_profit_loss_percent.return_value = -0.5
+    position_mocker.intraday_profit_profit_percent.return_value = -0.5
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss_intraday', '1%', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit_intraday', '1%', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is False
 
 
@@ -72,13 +72,13 @@ def test_check_trigger_intraday_percent_not_hit_case(data_mocker, position_mocke
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_intraday_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.intraday_profit_loss.return_value = -1500
+    position_mocker.intraday_profit_profit.return_value = -1500
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss_intraday', '1000', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit_intraday', '1000', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is True
 
 
@@ -86,13 +86,13 @@ def test_check_trigger_intraday_hit_case(data_mocker, position_mocker, test_obje
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_intraday_not_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.intraday_profit_loss.return_value = -150
+    position_mocker.intraday_profit_profit.return_value = -150
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss_intraday', '1000', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit_intraday', '1000', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is False
 
 
@@ -100,13 +100,13 @@ def test_check_trigger_intraday_not_hit_case(data_mocker, position_mocker, test_
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_lifetime_percent_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.profit_loss_percent.return_value = -1.1
+    position_mocker.profit_profit_percent.return_value = -1.1
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss', '1%', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit', '1%', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is True
 
 
@@ -114,13 +114,13 @@ def test_check_trigger_lifetime_percent_hit_case(data_mocker, position_mocker, t
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_lifetime_percent_not_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.profit_loss_percent.return_value = -0.1
+    position_mocker.profit_profit_percent.return_value = -0.1
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss', '1%', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit', '1%', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is False
 
 
@@ -128,13 +128,13 @@ def test_check_trigger_lifetime_percent_not_hit_case(data_mocker, position_mocke
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_lifetime_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.profit_loss.return_value = -1500
+    position_mocker.profit_profit.return_value = -1500
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss', '1000', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit', '1000', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is True
 
 
@@ -142,11 +142,11 @@ def test_check_trigger_lifetime_hit_case(data_mocker, position_mocker, test_obje
 @patch('StockBench.simulation_data.data_manager.DataManager')
 def test_check_trigger_lifetime_not_hit_case(data_mocker, position_mocker, test_object):
     # ============= Arrange ==============
-    position_mocker.profit_loss.return_value = -100
+    position_mocker.profit_profit.return_value = -100
 
     # ============= Act ==================
-    actual = test_object.check_trigger('stop_loss', '1000', data_mocker, position_mocker, 0)
+    actual = test_object.check_trigger('stop_profit', '1000', data_mocker, position_mocker, 0)
 
     # ============= Assert ===============
-    assert position_mocker.profit_loss_percent.called is True
+    assert position_mocker.profit_profit_percent.called is True
     assert actual is False

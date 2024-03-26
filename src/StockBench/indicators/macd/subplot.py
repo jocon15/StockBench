549452a532e0d277267016e0572559f9d9a55b74
dev sys <jaywoc12@gmail.com>
2024-03-26 17:04:36 -0400

    updated comments for macd indicator

--- src/StockBench/indicators/macd/subplot.py
+++ src/StockBench/indicators/macd/subplot.py
@@ -3,16 +3,33 @@ from StockBench.indicator.subplot import Subplot
 
 
 class MACDSubplot(Subplot):
-    """"""
+    """Subplot for the MACD indicator.
+
+    Additional traces include:
+    """
     def __init__(self):
         super().__init__('MACD', [{"type": "bar"}], False)
 
     @staticmethod
     def get_subplot(df):
-        """"""
+        """Builds the subplot.
+
+        Args:
+            df (DataFrame): The dataframe from the simulation.
+
+        return:
+            A plotly subplot.
+        """
         return fplt.Bar(x=df['Date'], y=df['MACD'], name='MACD')
 
     @staticmethod
-    def get_traces(self) -> list:
-        """"""
+    def get_traces(df) -> list:
+        """Build a list of traces to add to the subplot.
+
+        Args:
+            df (DataFrame): The dataframe from the simulation.
+
+        return:
+            list: A list of traces to add to the subplot defined in this class.
+        """
         return []

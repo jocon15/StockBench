e4b2134ab45e2a03421264209308dc269edadc80
dev sys <jaywoc12@gmail.com>
2024-05-18 17:14:18 -0400

    strategy studio loads filepath if a valid one is passed or loads a template strategy

--- src/StockBench/gui/windows/StrategyEditor.py
+++ src/StockBench/gui/windows/StrategyEditor.py
@@ -2,9 +2,18 @@ import json
 
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QPushButton
 
+TEMPLATE_JSON = {
+    "buy": {
+
+    },
+    "sell": {
+
+    }
+}
+
 
 class StrategyEditorWindow(QWidget):
-    def __init__(self):
+    def __init__(self, filepath):
         super().__init__()
 
         self.layout = QVBoxLayout()
@@ -19,13 +28,22 @@ class StrategyEditorWindow(QWidget):
         self.setGeometry(1000, 10, 1000, 1000)
 
         # load the strategy from the file
-        with open('test.json', 'r') as file:
-            json_data = json.load(file)
-
-        # set the strategy text to the editor widget
-        self.text_edit.setText(json.dumps(json_data, indent=4))
+        self.load_filepath_into_editor(filepath)
 
         self.setLayout(self.layout)
+
+    def load_filepath_into_editor(self, filepath):
+        # if a filepath was injected, try loading it, else, show the template
+        if filepath is not None and filepath != '':
+            # load the strategy from the file
+            with open('test.json', 'r') as file:
+                json_data = json.load(file)
+
+            # set the strategy text to the editor widget
+            self.text_edit.setText(json.dumps(json_data, indent=4))
+        else:
+            # set the strategy text to the editor widget
+            self.text_edit.setText(json.dumps(TEMPLATE_JSON, indent=4))
 
     def done_btn_clicked(self):
         pass

b0a680db413bd72842f4baebab76f48b58759ff9
dev sys <jaywoc12@gmail.com>
2024-07-05 22:47:06 -0400

    built config tab for head to head

--- src/StockBench/gui/application.py
+++ src/StockBench/gui/application.py
@@ -20,7 +20,7 @@ from StockBench.gui.windows.head_to_head.head_to_head_window import HeadToHeadWi
 
 class ConfigMainWindow(QMainWindow):
     WIDTH = 400
-    HEIGHT = 650
+    HEIGHT = 750
 
     def __init__(self, splash):
         super().__init__()
@@ -37,6 +37,7 @@ class ConfigMainWindow(QMainWindow):
         self.tab_widget = QTabWidget()
         self.tab_widget.addTab(SingularConfigTab(), "Single")
         self.tab_widget.addTab(MultiConfigTab(), "Multi")
+        self.tab_widget.addTab(HeadToHeadConfigTab(), "Compare")
         self.tab_widget.setStyleSheet(Palette.TAB_WIDGET_STYLESHEET)
         self.layout.addWidget(self.tab_widget)
 
@@ -581,20 +582,229 @@ class HeadToHeadConfigTab(ConfigTab):
         # layout type
         self.layout = QVBoxLayout()
 
-        self.run_multiple_btn = QPushButton()
-        self.run_multiple_btn.setFixedSize(60, 30)
-        self.run_multiple_btn.setText('RUN MULTIPLE')
-        self.run_multiple_btn.clicked.connect(self.on_run_multiple_btn_clicked)  # noqa
-        self.run_multiple_btn.setStyleSheet(Palette.RUN_BTN_STYLESHEET)
-        self.layout.addWidget(self.run_multiple_btn, alignment=Qt.AlignmentFlag.AlignRight)
+        label = QLabel()
+        label.setText('Strategy 1:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.strategy_1_selection_box = StrategySelection()
+        self.strategy_1_selection_box.setStyleSheet(Palette.SELECT_FILE_BTN_STYLESHEET)
+        self.layout.addWidget(self.strategy_1_selection_box)
+        self.strategy_1_studio_btn = QPushButton()
+        self.strategy_1_studio_btn.setText('Strategy Studio (beta)')
+        self.strategy_1_studio_btn.clicked.connect(self.on_strategy_1_studio_btn_clicked)  # noqa
+        self.strategy_1_studio_btn.setStyleSheet(Palette.SELECT_FILE_BTN_STYLESHEET)
+        self.layout.addWidget(self.strategy_1_studio_btn)
+
+        label = QLabel()
+        label.setText('Strategy 2:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.strategy_2_selection_box = StrategySelection()
+        self.strategy_2_selection_box.setStyleSheet(Palette.SELECT_FILE_BTN_STYLESHEET)
+        self.layout.addWidget(self.strategy_2_selection_box)
+        self.strategy_2_studio_btn = QPushButton()
+        self.strategy_2_studio_btn.setText('Strategy Studio (beta)')
+        self.strategy_2_studio_btn.clicked.connect(self.on_strategy_2_studio_btn_clicked)  # noqa
+        self.strategy_2_studio_btn.setStyleSheet(Palette.SELECT_FILE_BTN_STYLESHEET)
+        self.layout.addWidget(self.strategy_2_studio_btn)
+
+        label = QLabel()
+        label.setText('Simulation Length:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.simulation_length_cbox = QComboBox()
+        self.simulation_length_cbox.addItem('1 Year')
+        self.simulation_length_cbox.addItem('2 Year')
+        self.simulation_length_cbox.addItem('5 Year')
+        # set simulation length default to 2 years (must set attribute as well)
+        self.simulation_length_cbox.setCurrentIndex(1)
+        self.simulation_length = SECONDS_2_YEAR
+
+        self.simulation_length_cbox.setStyleSheet(Palette.COMBOBOX_STYLESHEET)
+        self.simulation_length_cbox.currentIndexChanged.connect(self.on_simulation_length_cbox_index_changed)  # noqa
+        self.layout.addWidget(self.simulation_length_cbox)
+
+        label = QLabel()
+        label.setText('Simulation Symbols:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.symbol_tbox = QLineEdit()
+        self.symbol_tbox.setText("MSFT, AAPL")
+        self.symbol_tbox.setStyleSheet(Palette.LINE_EDIT_STYLESHEET)
+        self.layout.addWidget(self.symbol_tbox)
+
+        label = QLabel()
+        label.setText('Initial Balance:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.balance_tbox = QLineEdit()
+        self.balance_tbox.setText('1000.0')
+        self.onlyFloat = QDoubleValidator()
+        self.balance_tbox.setValidator(self.onlyFloat)
+        self.balance_tbox.setStyleSheet(Palette.LINE_EDIT_STYLESHEET)
+        self.layout.addWidget(self.balance_tbox)
+
+        label = QLabel()
+        label.setText('Logging:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.logging_btn = QPushButton()
+        self.logging_btn.setCheckable(True)
+        self.logging_btn.setText('OFF')
+        self.logging_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+        self.logging_btn.clicked.connect(self.on_logging_btn_clicked)  # noqa
+        self.layout.addWidget(self.logging_btn)
+
+        label = QLabel()
+        label.setText('Reporting:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.reporting_btn = QPushButton()
+        self.reporting_btn.setCheckable(True)
+        self.reporting_btn.setText('OFF')
+        self.reporting_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+        self.reporting_btn.clicked.connect(self.on_reporting_btn_clicked)  # noqa
+        self.layout.addWidget(self.reporting_btn)
+
+        label = QLabel()
+        label.setText('Save Unique Charts:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.unique_chart_save_btn = QPushButton()
+        self.unique_chart_save_btn.setCheckable(True)
+        self.unique_chart_save_btn.setText('OFF')
+        self.unique_chart_save_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+        self.unique_chart_save_btn.clicked.connect(self.on_chart_saving_btn_clicked)  # noqa
+        self.layout.addWidget(self.unique_chart_save_btn)
+
+        label = QLabel()
+        label.setText('Show Results:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.show_sim_results_btn = QPushButton()
+        self.show_sim_results_btn.setCheckable(True)
+        self.show_sim_results_btn.setText('ON')
+        self.show_sim_results_btn.setStyleSheet(Palette.TOGGLE_BTN_ENABLED_STYLESHEET)
+        self.show_sim_results_btn.clicked.connect(self.on_show_results_btn_clicked)  # noqa
+        self.layout.addWidget(self.show_sim_results_btn)
+
+        label = QLabel()
+        label.setText('Results Depth:')
+        label.setStyleSheet("""color: #FFF;""")
+        self.layout.addWidget(label)
+
+        self.data_and_charts_radio_btn = QRadioButton("Data and Charts")
+        self.data_and_charts_radio_btn.toggled.connect(self.data_and_charts_btn_selected)  # noqa
+        self.data_and_charts_radio_btn.setStyleSheet(Palette.RADIO_BTN_STYLESHEET)
+        self.data_and_charts_radio_btn.toggle()  # set data and charts as default
+        self.layout.addWidget(self.data_and_charts_radio_btn)
+        self.data_only_radio_btn = QRadioButton("Data Only")
+        self.data_only_radio_btn.toggled.connect(self.data_only_btn_selected)  # noqa
+        self.layout.addWidget(self.data_only_radio_btn)
+        self.data_only_radio_btn.setStyleSheet(Palette.RADIO_BTN_STYLESHEET)
+
+        self.run_btn = QPushButton()
+        self.run_btn.setFixedSize(60, 30)
+        self.run_btn.setText('RUN')
+        self.run_btn.clicked.connect(self.on_run_multiple_btn_clicked)  # noqa
+        self.run_btn.setStyleSheet(Palette.RUN_BTN_STYLESHEET)
+        self.layout.addWidget(self.run_btn, alignment=Qt.AlignmentFlag.AlignRight)
+
+        self.error_message_box = QLabel()
+        self.error_message_box.setStyleSheet(Palette.ERROR_LABEL_STYLESHEET)
+        self.layout.addWidget(self.error_message_box)
 
         # add the layout to the widget
         self.setLayout(self.layout)
 
+    def on_strategy_1_studio_btn_clicked(self):
+        # launch the strategy studio window injecting the filepath from the filepath box into the strategy editor
+        coordinates = self.mapToGlobal(QPoint(0, 0))
+        self.strategy_studio_window = StrategyStudioWindow(
+            self.strategy_1_selection_box.filepath_box.text(),
+            coordinates,
+            self.width()
+        )
+        self.strategy_studio_window.show()
+
+    def on_strategy_2_studio_btn_clicked(self):
+        # launch the strategy studio window injecting the filepath from the filepath box into the strategy editor
+        coordinates = self.mapToGlobal(QPoint(0, 0))
+        self.strategy_studio_window = StrategyStudioWindow(
+            self.strategy_2_selection_box.filepath_box.text(),
+            coordinates,
+            self.width()
+        )
+        self.strategy_studio_window.show()
+
+    def on_logging_btn_clicked(self):
+        if self.logging_btn.isChecked():
+            self.simulation_logging = True
+            self.logging_btn.setText('ON')
+            self.logging_btn.setStyleSheet(Palette.TOGGLE_BTN_ENABLED_STYLESHEET)
+        else:
+            self.simulation_logging = False
+            self.logging_btn.setText('OFF')
+            self.logging_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+
+    def on_reporting_btn_clicked(self):
+        if self.reporting_btn.isChecked():
+            self.simulation_reporting = True
+            self.reporting_btn.setText('ON')
+            self.reporting_btn.setStyleSheet(Palette.TOGGLE_BTN_ENABLED_STYLESHEET)
+        else:
+            self.simulation_reporting = False
+            self.reporting_btn.setText('OFF')
+            self.reporting_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+
+    def on_chart_saving_btn_clicked(self):
+        if self.unique_chart_save_btn.isChecked():
+            self.simulation_unique_chart_saving = True
+            self.unique_chart_save_btn.setText('ON')
+            self.unique_chart_save_btn.setStyleSheet(Palette.TOGGLE_BTN_ENABLED_STYLESHEET)
+        else:
+            self.simulation_unique_chart_saving = False
+            self.unique_chart_save_btn.setText('OFF')
+            self.unique_chart_save_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+
+    def on_show_results_btn_clicked(self):
+        if self.show_sim_results_btn.isChecked():
+            self.simulation_show_results_window = True
+            self.show_sim_results_btn.setText('ON')
+            self.show_sim_results_btn.setStyleSheet(Palette.TOGGLE_BTN_ENABLED_STYLESHEET)
+        else:
+            self.simulation_show_results_window = False
+            self.show_sim_results_btn.setText('OFF')
+            self.show_sim_results_btn.setStyleSheet(Palette.TOGGLE_BTN_DISABLED_STYLESHEET)
+
+    def on_simulation_length_cbox_index_changed(self, index):
+        if index == 0:
+            self.simulation_length = SECONDS_1_YEAR
+        elif index == 1:
+            self.simulation_length = SECONDS_2_YEAR
+        elif index == 2:
+            self.simulation_length = SECONDS_5_YEAR
+
+    def data_and_charts_btn_selected(self, selected):
+        if selected:
+            self.results_depth = Simulator.CHARTS_AND_DATA
+
+    def data_only_btn_selected(self, selected):
+        if selected:
+            self.results_depth = Simulator.DATA_ONLY
 
     def on_run_multiple_btn_clicked(self):
         # load the strategy from the JSON file into a strategy python dict
-        strategy_filepath = self.strategy_selection_box.filepath_box.text()
+        strategy_filepath = self.strategy_1_selection_box.filepath_box.text()
         if strategy_filepath is None or strategy_filepath == '':
             self.error_message_box.setText('You must select a strategy file!')
             return

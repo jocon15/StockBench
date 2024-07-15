cebc03b8ff0b0520315b874f107b7659ba590ac6
dev sys <jaywoc12@gmail.com>
2024-07-14 23:43:05 -0400

    colored cell text white

--- src/StockBench/gui/windows/folder/folder_window.py
+++ src/StockBench/gui/windows/folder/folder_window.py
@@ -1,9 +1,14 @@
 from PyQt6.QtWidgets import QProgressBar, QLabel, QTableWidget, QTableWidgetItem
+from PyQt6.QtGui import QBrush, QColor
 from StockBench.gui.palette.palette import Palette
 from StockBench.gui.windows.base.results_window import SimulationResultsWindow
 
 
 class FolderResultsWindow(SimulationResultsWindow):
+    TABLE_HEADERS = ['strategy', 'Trades Made', 'Effectiveness', 'Total P/L', 'Average P/L', 'Median P/L', 'Stddev']
+
+    CELL_TEXT_COLOR = QColor(255, 255, 255)
+
     def __init__(self, strategies, symbols, initial_balance, simulator, progress_observer, worker, logging_on,
                  reporting_on, unique_chart_saving_on, results_depth):
         # pass 1st strategy as a dummy strategy because we will load the strategy dynamically in _run_simulation
@@ -35,6 +40,7 @@ class FolderResultsWindow(SimulationResultsWindow):
         self.table = QTableWidget()
         self.table.setRowCount(len(strategies))
         self.table.setColumnCount(7)
+        self.table.setHorizontalHeaderLabels(self.TABLE_HEADERS)
         self.layout.addWidget(self.table)
 
         # error message
@@ -91,10 +97,26 @@ class FolderResultsWindow(SimulationResultsWindow):
         results = simulation_results['results']
 
         for row, result in enumerate(results):
-            self.table.setItem(row, 0, QTableWidgetItem(str(result['strategy'])))
-            self.table.setItem(row, 1, QTableWidgetItem(str(result['trades_made'])))
-            self.table.setItem(row, 2, QTableWidgetItem(str(result['effectiveness'])))
-            self.table.setItem(row, 3, QTableWidgetItem(str(result['total_profit_loss'])))
-            self.table.setItem(row, 4, QTableWidgetItem(str(result['average_profit_loss'])))
-            self.table.setItem(row, 5, QTableWidgetItem(str(result['median_profit_loss'])))
-            self.table.setItem(row, 6, QTableWidgetItem(str(result['standard_profit_loss_deviation'])))
+            strategy_cell = QTableWidgetItem(str(result['strategy']))
+            trades_made_cell = QTableWidgetItem(str(result['trades_made']))
+            effectiveness_cell = QTableWidgetItem(str(result['effectiveness']))
+            total_pl_cell = QTableWidgetItem(str(result['total_profit_loss']))
+            avg_pl_cell = QTableWidgetItem(str(result['average_profit_loss']))
+            median_pl_cell = QTableWidgetItem(str(result['median_profit_loss']))
+            stddev_pl_cell = QTableWidgetItem(str(result['standard_profit_loss_deviation']))
+
+            strategy_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+            trades_made_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+            effectiveness_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+            total_pl_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+            avg_pl_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+            median_pl_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+            stddev_pl_cell.setForeground(QBrush(self.CELL_TEXT_COLOR))
+
+            self.table.setItem(row, 0, strategy_cell)
+            self.table.setItem(row, 1, trades_made_cell)
+            self.table.setItem(row, 2, effectiveness_cell)
+            self.table.setItem(row, 3, total_pl_cell)
+            self.table.setItem(row, 4, avg_pl_cell)
+            self.table.setItem(row, 5, median_pl_cell)
+            self.table.setItem(row, 6, stddev_pl_cell)

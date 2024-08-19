a8a8d7fb186383f5b780dbac8447693ab5c77b4a
dev sys <jaywoc12@gmail.com>
2024-08-19 00:07:25 -0400

    added helper function for creating bar charts in folder charting engine

--- src/StockBench/charting/folder/folder_charting_engine.py
+++ src/StockBench/charting/folder/folder_charting_engine.py
@@ -10,20 +10,6 @@ class FolderChartingEngine(ChartingEngine):
     @staticmethod
     def build_trades_made_chart(folder_results: list) -> str:
         """Build a chart for trades made."""
-        rows = 2
-        cols = 1
-
-        chart_list = [[{"type": "bar"}], [{"type": "bar"}]]
-        chart_titles = ('Trades Made per Strategy',)
-
-        # Parent Plot
-        fig = make_subplots(rows=rows,
-                            cols=cols,
-                            shared_xaxes=True,
-                            vertical_spacing=0.15,
-                            horizontal_spacing=0.05,
-                            specs=chart_list,
-                            subplot_titles=chart_titles)
 
         strategy_names = []
         trades_made_data = []
@@ -31,17 +17,34 @@ class FolderChartingEngine(ChartingEngine):
             strategy_names.append(result['strategy'])
             trades_made_data.append(float(result['trades_made']))
 
+        formatted_fig = FolderChartingEngine._build_simple_bar_chart(strategy_names, trades_made_data,
+                                                                     'Trades Made per Strategy', OFF_BLUE)
+
+        # perform and saving or showing (returns saved filepath)
+        return ChartingEngine.handle_save_chart(formatted_fig, ChartingEngine.TEMP_SAVE,
+                                                'temp_folder_trades_made', f'')
+
+    @staticmethod
+    def _build_simple_bar_chart(x_values: list, y_values: list, title: str, marker_color: str) -> str:
+        """Build a simple bar chart."""
+        rows = 1
+        cols = 1
+
+        chart_list = [[{"type": "bar"}]]
+        chart_titles = (title,)
+
+        # Parent Plot
+        fig = make_subplots(rows=rows, cols=cols, shared_xaxes=True, vertical_spacing=0.15, horizontal_spacing=0.05,
+                            specs=chart_list, subplot_titles=chart_titles)
+
         # rule counts chart
         fig.add_trace(plotter.Bar(
-            x=strategy_names,
-            y=trades_made_data,
-            marker=dict(color=OFF_BLUE), name='Count'), 1, 1)
+            x=x_values,
+            y=y_values,
+            marker=dict(color=marker_color), name='Count'), 1, 1)
 
         # set the layout
         fig.update_layout(template='plotly_dark', xaxis_rangeslider_visible=False)
 
         # format the chart (remove plotly white border)
-        formatted_fig = ChartingEngine.format_chart(fig)
-
-        # perform and saving or showing (returns saved filepath)
-        return ChartingEngine.handle_save_chart(formatted_fig, ChartingEngine.TEMP_SAVE, 'temp_folder_trades_made', f'')
+        return ChartingEngine.format_chart(fig)

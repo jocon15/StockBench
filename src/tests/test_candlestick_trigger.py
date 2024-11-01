70fb3fa18285a7d677f04ed513b86a1566c754d2
Jason O <jaywoc12@gmail.com>
2024-11-01 16:42:37 -0400

    fixed color tests

--- src/tests/test_candlestick_trigger.py
+++ src/tests/test_candlestick_trigger.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch
 from StockBench.indicators.candlestick_color.trigger import CandlestickColorTrigger
+from StockBench.indicator.exceptions import StrategyIndicatorError
 
 
 @pytest.fixture
@@ -8,7 +9,7 @@ def test_object():
     return CandlestickColorTrigger('color')
 
 
-def test_additional_days(test_object):
+def test_additional_days_normal(test_object):
     # ============= Arrange ==============
 
     # ============= Act ==================
@@ -17,6 +18,19 @@ def test_additional_days(test_object):
     # ============= Assert ===============
     assert type(actual) is int
     assert actual == 1
+
+
+def test_additional_days_no_values(test_object):
+    # ============= Arrange ==============
+
+    # ============= Act ==================
+
+    # ============= Assert ===============
+    try:
+        test_object.additional_days('color', {})
+        assert False
+    except StrategyIndicatorError:
+        assert True
 
 
 def test_add_to_data(test_object):
@@ -44,15 +58,29 @@ def test_check_trigger(data_mocker, test_object):
     # ============= Act ==================
 
     # ============= Assert ===============
-    assert test_object.check_trigger('color', {'0': 'green', '1': 'red'},
-                                     data_mocker,
-                                     None,
+    assert test_object.check_trigger('color', {'0': 'green', '1': 'red'}, data_mocker,None,
                                      current_day_index) is True
 
-    assert test_object.check_trigger('color', {'0': 'red', '1': 'green'},
-                                     data_mocker,
-                                     None,
+    assert test_object.check_trigger('color', {'0': 'red', '1': 'green'}, data_mocker,None,
                                      current_day_index) is False
+
+
+@patch('StockBench.simulation_data.data_manager.DataManager')
+def test_check_trigger_no_values(data_mocker, test_object):
+    # ============= Arrange ==============
+    current_day_index = 0
+
+    data_mocker.COLOR = "color"
+    data_mocker.get_data_point.side_effect = mock_colors_even
+
+    # ============= Act ==================
+
+    # ============= Assert ===============
+    try:
+        test_object.check_trigger('color', {}, data_mocker, None, current_day_index)
+        assert False
+    except StrategyIndicatorError:
+        assert True
 
 
 def mock_colors_even(*args):

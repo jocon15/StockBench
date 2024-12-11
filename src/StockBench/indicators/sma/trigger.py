f5f794bf50d210038833d2ba721e632feabbb940
Jason O <jaywoc12@gmail.com>
2024-12-11 00:07:00 -0500

    sma trigger cleanup

--- src/StockBench/indicators/sma/trigger.py
+++ src/StockBench/indicators/sma/trigger.py
@@ -3,6 +3,8 @@ import statistics
 from StockBench.constants import *
 from StockBench.indicator.trigger import Trigger
 from StockBench.indicator.exceptions import StrategyIndicatorError
+from StockBench.simulation_data.data_manager import DataManager
+from StockBench.position.position import Position
 
 log = logging.getLogger()
 
@@ -19,11 +21,10 @@ class SMATrigger(Trigger):
             value_value (any): The value from the strategy.
         """
         # map nums to a list of ints
-        nums = list(map(int, self.find_all_nums_in_str(rule_key)))
-        if nums:
-            return max(nums)
-        # nums is empty
-        raise StrategyIndicatorError(f'{self.strategy_symbol} key: {rule_key} must have an indicator length!')
+        rule_key_number_groupings = list(map(int, self.find_all_nums_in_str(rule_key)))
+        if rule_key_number_groupings:
+            return max(rule_key_number_groupings)
+        raise StrategyIndicatorError(f'{self.strategy_symbol} rule key: {rule_key} must have an indicator length!')
 
     def add_to_data(self, rule_key, rule_value, side, data_manager):
         """Add data to the dataframe.
@@ -32,16 +33,14 @@ class SMATrigger(Trigger):
             rule_key (any): The key value from the strategy.
             rule_value (any): The value from thr strategy.
             side (str): The side (buy/sell).
-            data_manager (any): The data object.
+            data_manager (DataManager): The data object.
         """
         nums = self.find_all_nums_in_str(rule_key)
         if len(nums) > 0:
-            # element 0 will be the indicator length
             indicator_length = int(nums[0])
-            # add the EMA data to the df
             self.__add_sma(indicator_length, data_manager)
         else:
-            raise StrategyIndicatorError(f'{self.strategy_symbol} key: {rule_key} must have an indicator length!')
+            raise StrategyIndicatorError(f'{self.strategy_symbol} rule key: {rule_key} must have an indicator length!')
 
     def check_trigger(self, rule_key, rule_value, data_manager, position, current_day_index) -> bool:
         """Trigger logic for SMA.
@@ -49,8 +48,8 @@ class SMATrigger(Trigger):
         Args:
             rule_key (str): The key value of the algorithm.
             rule_value (str): The value of the algorithm.
-            data_manager (any): The data API object.
-            position (any): The position object.
+            data_manager (DataManager): The data API object.
+            position (Position): The position object.
             current_day_index (int): The index of the current day.
 
         return:
@@ -58,64 +57,52 @@ class SMATrigger(Trigger):
         """
         log.debug(f'Checking {self.strategy_symbol} algorithm: {rule_key}...')
 
-        # get the indicator value from the key
         indicator_value = self.__parse_key(rule_key, data_manager, current_day_index)
 
-        # get the operator and algorithm value from the value
         operator, trigger_value = self._parse_rule_value(rule_value, data_manager, current_day_index)
 
         log.debug(f'{self.strategy_symbol} algorithm: {rule_key} checked successfully')
 
-        # algorithm checks
         return Trigger.basic_trigger_check(indicator_value, operator, trigger_value)
 
-    def __parse_key(self, key, data_manager, current_day_index) -> float:
+    def __parse_key(self, rule_key: any, data_manager: DataManager, current_day_index: int) -> float:
         """Parser for parsing the key into the indicator value."""
         # find the indicator value (left hand side of the comparison)
-        nums = self.find_all_nums_in_str(key)
+        rule_key_number_groupings = self.find_all_nums_in_str(rule_key)
 
         # do not build title outside of conditional as nums could be [] which would result in index error
 
-        if len(nums) == 1:
-            if SLOPE_SYMBOL in key:
-                raise StrategyIndicatorError(f'{self.strategy_symbol} key: {key} does not contain enough number '
-                                             f'groupings!')
-            # title of the column in the data
-            title = f'{self.strategy_symbol}{int(nums[0])}'
+        if len(rule_key_number_groupings) == 1:
+            if SLOPE_SYMBOL in rule_key:
+                raise StrategyIndicatorError(f'{self.strategy_symbol} rule key: {rule_key} does not contain '
+                                             f'enough number groupings!')
+            title = f'{self.strategy_symbol}{int(rule_key_number_groupings[0])}'
             indicator_value = float(data_manager.get_data_point(title, current_day_index))
-        elif len(nums) == 2:
-            # title of the column in the data
-            title = f'{self.strategy_symbol}{int(nums[0])}'
+        elif len(rule_key_number_groupings) == 2:
+            title = f'{self.strategy_symbol}{int(rule_key_number_groupings[0])}'
             # likely that the $slope indicator is being used
-            if SLOPE_SYMBOL in key:
-                # get the length of the slope window
-                slope_window_length = int(nums[1])
+            if SLOPE_SYMBOL in rule_key:
+                slope_window_length = int(rule_key_number_groupings[1])
 
                 # data request length is window - 1 to account for the current day index being a part of the window
                 slope_data_request_length = slope_window_length - 1
 
-                # calculate slope
                 indicator_value = self.calculate_slope(
                     float(data_manager.get_data_point(title, current_day_index)),
                     float(data_manager.get_data_point(title, current_day_index - slope_data_request_length)),
                     slope_window_length
                 )
             else:
-                raise StrategyIndicatorError(f'{self.strategy_symbol} key: {key} contains too many number groupings! '
-                                             f'Are you missing a $slope emblem?')
+                raise StrategyIndicatorError(f'{self.strategy_symbol} rule key: {rule_key} contains '
+                                             f'too many number groupings! Are you missing a $slope emblem?')
         else:
-            raise StrategyIndicatorError(f'{self.strategy_symbol} key: {key} contains too many number groupings!')
+            raise StrategyIndicatorError(f'{self.strategy_symbol} rule key: {rule_key} contains '
+                                         f'invalid number groupings!')
 
         return indicator_value
 
-    def __add_sma(self, length, data_manager):
-        """Pre-calculate the SMA values and add them to the df.
-
-        Args:
-            length (int): The length of the SMA to use.
-            data_manager (any): The data object.
-        """
-        # get a list of close price values
+    def __add_sma(self, length: int, data_manager: DataManager):
+        """Pre-calculate the SMA values and add them to the df."""
         column_title = f'{self.strategy_symbol}{length}'
 
         # if SMA values ar already in the df, we don't need to add them again
@@ -123,29 +110,15 @@ class SMATrigger(Trigger):
             if column_title in col_name:
                 return
 
-        # get a list of price values as a list
         price_data = data_manager.get_column_data(data_manager.CLOSE)
 
-        # calculate the SMA values from the indicator API
         sma_values = SMATrigger.__calculate_sma(length, price_data)
 
-        # add the calculated values to the df
         data_manager.add_column(column_title, sma_values)
 
     @staticmethod
     def __calculate_sma(length: int, price_data: list) -> list:
-        """Calculates the SMA values for a list of price values.
-
-        Args:
-            length (int): The length of the SMA to calculate.
-            price_data (list): The price data to calculate the SMA from.
-
-        return:
-            list: The list of calculated SMA values.
-
-        Notes:
-            This requires you to keep track of all SMA values so that all values can be added to the df.
-        """
+        """Calculates the SMA values for a list of price values."""
         price_values = []
         sma_values = []
         all_sma_values = []

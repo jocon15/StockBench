59bdb8dd91ce56faf04d996b82d8734481411efd
Jason O <jaywoc12@gmail.com>
2025-01-11 16:02:44 -0500

    fixed docstring

--- src/StockBench/indicators/ema/trigger.py
+++ src/StockBench/indicators/ema/trigger.py
@@ -51,7 +51,7 @@ class EMATrigger(Trigger):
         return self.add_to_data_from_rule_key(rule_value, None, side, data_manager)
 
     def get_value_when_referenced(self, rule_value: str, data_manager: DataManager, current_day_index) -> float:
-        """Get the value of the indicator when referenced in a rule value"""
+        """Get the value of the indicator when referenced in a rule value."""
         # parse rule key will work even when passed a rule value
         return Trigger._parse_rule_key_no_default_indicator_length(rule_value, self.indicator_symbol, data_manager,
                                                                    current_day_index)

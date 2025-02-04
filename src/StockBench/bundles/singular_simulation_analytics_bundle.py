c13728f535c399158d15d9abecf87cd35326818f
Jason O <jaywoc12@gmail.com>
2025-02-03 22:03:56 -0500

    created bundle data classes

---
+++ src/StockBench/bundles/singular_simulation_analytics_bundle.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SingularSimulationAnalyticsBundle:
+    """Encapsulates a set of analytic chart filepaths."""
+    overview_chart_filepath: str
+    buy_rules_chart_filepath: str
+    sell_rules_chart_filepath: str
+    account_value_bar_chart_filepath: str
+    positions_duration_bar_chart_filepath: str
+    positions_profit_loss_bar_chart_filepath: str
+    positions_profit_loss_histogram_chart_filepath: str

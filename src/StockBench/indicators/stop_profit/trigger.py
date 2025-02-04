0ea7ba3064c3838f2afa1bf5f908426206382b08
Jason O <jaywoc12@gmail.com>
2025-02-03 21:29:01 -0500

    cleaned stop profit trigger

--- src/StockBench/indicators/stop_profit/trigger.py
+++ src/StockBench/indicators/stop_profit/trigger.py
@@ -10,54 +10,24 @@ class StopProfitTrigger(Trigger):
     def __init__(self, indicator_symbol):
         super().__init__(indicator_symbol, side=Trigger.SELL)
 
-    def additional_days_from_rule_key(self, rule_key: str, rule_value) -> int:
-        """Calculate the additional days required from rule key.
-
-        Args:
-            rule_key: The key value from the strategy.
-            rule_value (any): The key value from the strategy (unused in this function).
-        """
-        # stop profit does not require additional days
+    def additional_days_from_rule_key(self, rule_key: str, rule_value: any) -> int:
         return 0
 
     def additional_days_from_rule_value(self, rule_value: any) -> int:
-        """Calculate the additional days required from rule value."""
-        # stop profit does not require additional days
         return 0
 
     def add_to_data_from_rule_key(self, rule_key: str, rule_value, side: str, data_manager: str):
-        """Add data to the dataframe from rule key.
-
-        Args:
-            rule_key (any): The key value from the strategy.
-            rule_value (any): The value from thr strategy.
-            side (str): The side (buy/sell).
-            data_manager (DataManager): The data object.
-        """
         # stop profit does not require any additional data to be added to the data
         return
 
     def add_to_data_from_rule_value(self, rule_value: str, side: str, data_manager: DataManager):
-        """Add data to the dataframe from rule value."""
         # stop profit does not require any additional data to be added to the data
         return
 
     def get_value_when_referenced(self, rule_value: str, data_manager: DataManager, current_day_index: int) -> float:
         raise NotImplementedError('Stop profit cannot be referenced in a rule value!')
 
-    def check_trigger(self, rule_key, rule_value, data_manager, position, current_day_index) -> bool:
-        """Trigger logic for stop profit.
-
-        Args:
-            rule_key (str): The key value of the algorithm.
-            rule_value (str): The value of the algorithm.
-            data_manager (DataManager): The data API object.
-            position (Position): The position object.
-            current_day_index (int): The index of the current day.
-
-        return:
-            bool: True if a trigger was hit.
-        """
+    def check_trigger(self, rule_key: str, rule_value: any, data_manager: DataManager, position: Position, current_day_index: int) -> bool:
         log.debug('Checking stop profit algorithm...')
 
         current_price = data_manager.get_data_point(data_manager.CLOSE, current_day_index)

64b3a482d71c2c86b75d2a498d9cf3c9abc6dfbc
Jason O'Connell <jaywoc12@gmail.com>
2025-10-16 11:50:34 -0400

    controller stubs for each simulation type

---
+++ src/StockBench/controllers/stockbench_controller.py
@@ -0,0 +1,14 @@
+
+
+class StockBenchController:
+    @staticmethod
+    def singular_simulation():
+        pass
+
+    @staticmethod
+    def multi_simulation():
+        pass
+
+    @staticmethod
+    def folder_simulation():
+        pass

5a442be81f5e03cf51f6dc18e091197988bc6c15
Jason O'Connell <jaywoc12@gmail.com>
2025-10-16 14:47:19 -0400

    condensed results window base class function with new return types

--- src/StockBench/gui/results/base/results_window.py
+++ src/StockBench/gui/results/base/results_window.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 import requests.exceptions
 
 from StockBench.controllers.simulator.broker.broker_client import InvalidSymbolError, InsufficientDataError, MissingCredentialError
-from StockBench.controllers.charting import ChartingEngine
+from StockBench.controllers.charting.charting_engine import ChartingEngine
 from PyQt6.QtWidgets import QWidget, QProgressBar, QTabWidget, QVBoxLayout
 from PyQt6.QtCore import QTimer, QThreadPool
 from PyQt6 import QtGui
@@ -18,6 +18,7 @@ from StockBench.controllers.simulator import Simulator
 from StockBench.controllers.simulator.indicator import StrategyIndicatorError
 from StockBench.controllers.charting.exceptions import ChartingError
 from StockBench.controllers.simulator.algorithm import MalformedStrategyError
+from StockBench.models.simulation_result.simulation_result import SimulationResult
 
 log = logging.getLogger()
 
@@ -102,56 +103,19 @@ class SimulationResultsWindow(QWidget):
             # update the progress bar
             self.progress_bar.setValue(int(self.progress_observer.get_progress()))
 
-    def __run_simulation(self) -> SimulationResultsBundle:
+    def __run_simulation(self) -> SimulationResult:
         """Run the simulation."""
+        result = self._run_simulation()
 
-        # FIXME: move all of this to the controller
-        # set up the simulator's configuration options
-        if self.logging:
-            self.simulator.enable_logging()
-        if self.reporting:
-            self.simulator.enable_reporting()
+        if not SimulationResult.simulation_successful(result.status_code):
+            # log all errors and display error message in console box
+            log.error(result.message)
+            self.overview_tab.update_error_message(result.message)
 
-        # configure the simulator's configuration options
-        if self.unique_chart_saving:
-            save_option = ChartingEngine.UNIQUE_SAVE
-        else:
-            save_option = ChartingEngine.TEMP_SAVE
-
-        # load the strategy file
-        self.simulator.load_strategy(self.strategy)
-
-        # run the simulation and catch any errors - keep the app from crashing even if the sim fails
-        try:
-            return self._run_simulation(save_option, self.results_depth)
-        except requests.exceptions.ConnectionError:
-            message = 'Failed to connect to broker!'
-        except MalformedStrategyError as e:
-            message = f'Malformed strategy error: {e}'
-        except StrategyIndicatorError as e:
-            message = f'Strategy error: {e}'
-        except ChartingError as e:
-            message = f'Charting error: {e}'
-        except MissingCredentialError as e:
-            message = f'Missing credential error: {e}'
-        except InvalidSymbolError as e:
-            message = f'Invalid symbol error: {e}'
-        except InsufficientDataError as e:
-            message = f'Insufficient data error {e}'
-        except Exception as e:
-            message = f'Unexpected error: {type(e)} {e} {traceback.print_exc()}'
-        # WARNING, trying to interact with UI components here (calling a custom abstract signature) will not work
-        # because the simulation is on a different QThread and will cause crash with no traceback, you must instead take
-        # any simulation failure action in the _render_data implementation because _render_data is the callback called
-        # by the QThread when it is done, success or failure.
-
-        # log all errors and display error message in console box
-        log.error(message)
-        self.overview_tab.update_error_message(message)
-        return SimulationResultsBundle({}, {})
+        return result
 
     @abstractmethod
-    def _run_simulation(self, save_option: int, results_depth: int) -> SimulationResultsBundle:
+    def _run_simulation(self) -> SimulationResult:
         raise NotImplementedError('You must define an implementation for _run_simulation()!')
 
     @abstractmethod

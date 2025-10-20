6fe14f2f7ce0ab1fc5f7dfd09ad4ad257e3f7945
Jason O'Connell <jaywoc12@gmail.com>
2025-10-20 13:51:59 -0400

    compare uses a secondary factory-created controller to allow the second multi window to use a different instance of the simulator

--- src/StockBench/gui/results/compare/compare_results_window.py
+++ src/StockBench/gui/results/compare/compare_results_window.py
@@ -2,6 +2,7 @@ from typing import List
 
 from PyQt6.QtWidgets import QWidget, QHBoxLayout
 
+from StockBench.controllers.controller_factory import StockBenchControllerFactory
 from StockBench.controllers.stockbench_controller import StockBenchController
 from StockBench.gui.palette.palette import Palette
 from StockBench.gui.results.multi.multi_results_window import MultiResultsWindow
@@ -24,19 +25,22 @@ class CompareResultsWindow(QWidget):
             simulation_logging,
             simulation_reporting,
             False,
-            results_depth,
-            identifier=1)
+            results_depth)
+
+        # NOTE: Since compare requires 2 different simulator instances, we need to create another controller instance
+        # using the factory, this time with an identifier of 2 so that the simulators do not use the same logger, and
+        # we do not create the issue where log messages are duplicated.
+        secondary_stockbench_controller = StockBenchControllerFactory.get_controller_instance(simulation_identifier=2)
 
         self.simulation_widget_2 = MultiResultsWindow(
-            stockbench_controller,
+            secondary_stockbench_controller,  # uses secondary controller instead
             simulation_symbols,
             strategy2,
             initial_balance,
             simulation_logging,
             simulation_reporting,
             True,  # prevent the second chart from loading the temp chart (being used by sim 1)
-            results_depth,
-            identifier=2)
+            results_depth)
 
         self.simulation_widget_1.begin()
 

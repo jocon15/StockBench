7a608340cfc947738b32686b43cb01fc4f894101
Jason O'Connell <jaywoc12@gmail.com>
2025-10-21 00:43:03 -0400

    condensed tests using fixtures

--- src/tests/test_simulation_proxy.py
+++ src/tests/test_simulation_proxy.py
@@ -1,5 +1,6 @@
-from unittest.mock import Mock, MagicMock
+from unittest.mock import MagicMock
 
+import pytest
 import requests
 
 from StockBench.controllers.proxies.simulation_proxy import SimulationProxy
@@ -8,14 +9,21 @@ from StockBench.controllers.simulator.broker.broker_client import MissingCredent
 from StockBench.controllers.simulator.indicator.exceptions import StrategyIndicatorError
 
 
-def test_run_singular_simulation_broker_error():
-    # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
-    mock_simulator.run.side_effect = requests.exceptions.ConnectionError
+@pytest.fixture
+def mock_simulator():
+    """Sets up a mocked simulator object."""
+    return MagicMock()
 
-    mock_progress_observer = Mock()
+
+@pytest.fixture
+def mock_progress_observer():
+    """Sets up a mocked progress observer object."""
+    return MagicMock()
+
+
+def test_run_singular_simulation_broker_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run.side_effect = requests.exceptions.ConnectionError
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -27,14 +35,9 @@ def test_run_singular_simulation_broker_error():
     assert result['message'] == 'Failed to connect to broker!'
 
 
-def test_run_singular_simulation_malformed_strategy_error():
+def test_run_singular_simulation_malformed_strategy_error(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.side_effect = MalformedStrategyError
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -46,14 +49,9 @@ def test_run_singular_simulation_malformed_strategy_error():
     assert result['message'] == 'Malformed strategy error: '
 
 
-def test_run_singular_simulation_strategy_indicator_error():
+def test_run_singular_simulation_strategy_indicator_error(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.side_effect = StrategyIndicatorError
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -65,14 +63,9 @@ def test_run_singular_simulation_strategy_indicator_error():
     assert result['message'] == 'Strategy error: '
 
 
-def test_run_singular_simulation_missing_credential_error():
+def test_run_singular_simulation_missing_credential_error(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.side_effect = MissingCredentialError
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -84,14 +77,9 @@ def test_run_singular_simulation_missing_credential_error():
     assert result['message'] == 'Missing credential error: '
 
 
-def test_run_singular_simulation_invalid_symbol_error():
+def test_run_singular_simulation_invalid_symbol_error(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.side_effect = MissingCredentialError
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -103,14 +91,9 @@ def test_run_singular_simulation_invalid_symbol_error():
     assert result['message'] == 'Missing credential error: '
 
 
-def test_run_singular_simulation_insufficient_data_error():
+def test_run_singular_simulation_insufficient_data_error(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.side_effect = InsufficientDataError
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -122,14 +105,9 @@ def test_run_singular_simulation_insufficient_data_error():
     assert result['message'] == 'Insufficient data error: '
 
 
-def test_run_singular_simulation_unexpected_error():
+def test_run_singular_simulation_unexpected_error(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
-    mock_simulator.run.side_effect = ValueError  # a random error not explicitly caught
-
-    mock_progress_observer = Mock()
+    mock_simulator.run.side_effect = ValueError  # a random error not explicitly caught by the decorator
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, False,
@@ -141,14 +119,9 @@ def test_run_singular_simulation_unexpected_error():
     assert 'Unexpected error: ' in result['message']
 
 
-def test_run_singular_simulation_normal_with_logging():
+def test_run_singular_simulation_normal_with_logging(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.return_value = {'symbol': 'AAPL', 'avg_pl': 200.1, 'med_pl': 40.1}
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, True, False,
@@ -165,14 +138,9 @@ def test_run_singular_simulation_normal_with_logging():
     assert result['symbol'] == 'AAPL'
 
 
-def test_run_singular_simulation_normal_with_reporting():
+def test_run_singular_simulation_normal_with_reporting(mock_simulator, mock_progress_observer):
     # ============= Arrange ==============
-    mock_simulator = MagicMock()
-    mock_simulator.set_initial_balance.return_value = None
-    mock_simulator.load_strategy.return_value = None
     mock_simulator.run.return_value = {'symbol': 'AAPL', 'avg_pl': 200.1, 'med_pl': 40.1}
-
-    mock_progress_observer = Mock()
 
     # ============= Act ==================
     result = SimulationProxy.run_singular_simulation(mock_simulator, {}, '', 0.0, False, True,

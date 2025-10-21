ac583f9d70f781f22630782d1933c15002edc1cc
Jason O'Connell <jaywoc12@gmail.com>
2025-10-21 13:11:05 -0400

    added tests for multi simulation proxy function

--- src/tests/test_simulation_proxy.py
+++ src/tests/test_simulation_proxy.py
@@ -159,4 +159,139 @@ def test_run_singular_simulation_normal_with_reporting(mock_simulator, mock_prog
 
 # FIXME: need tests for run_multi_simulation() function
 
+def test_run_multi_simulation_broker_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = requests.exceptions.ConnectionError
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert result['message'] == 'Failed to connect to broker!'
+
+
+def test_run_multi_simulation_malformed_strategy_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = MalformedStrategyError
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert result['message'] == 'Malformed strategy error: '
+
+
+def test_run_multi_simulation_strategy_indicator_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = StrategyIndicatorError
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert result['message'] == 'Strategy error: '
+
+
+def test_run_multi_simulation_missing_credential_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = MissingCredentialError
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert result['message'] == 'Missing credential error: '
+
+
+def test_run_multi_simulation_invalid_symbol_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = MissingCredentialError
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert result['message'] == 'Missing credential error: '
+
+
+def test_run_multi_simulation_insufficient_data_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = InsufficientDataError
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert result['message'] == 'Insufficient data error: '
+
+
+def test_run_multi_simulation_unexpected_error(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.side_effect = ValueError  # a random error not explicitly caught by the decorator
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    assert type(result) is dict
+    assert result['status_code'] == 400
+    assert 'Unexpected error: ' in result['message']
+
+
+def test_run_multi_simulation_normal_with_logging(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.return_value = {'symbol': 'AAPL', 'avg_pl': 200.1, 'med_pl': 40.1}
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, True, False,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    mock_simulator.enable_logging.assert_called_once()
+    mock_simulator.enable_reporting.assert_not_called()
+    mock_simulator.set_initial_balance.assert_called_once_with(0.0)
+    mock_simulator.load_strategy.assert_called_once_with({})
+
+    assert type(result) is dict
+    assert 'status_code' not in result.keys()
+    assert result['symbol'] == 'AAPL'
+
+
+def test_run_multi_simulation_normal_with_reporting(mock_simulator, mock_progress_observer):
+    # ============= Arrange ==============
+    mock_simulator.run_multiple.return_value = {'symbol': 'AAPL', 'avg_pl': 200.1, 'med_pl': 40.1}
+
+    # ============= Act ==================
+    result = SimulationProxy.run_multi_simulation(mock_simulator, {}, [''], 0.0, False, True,
+                                                  mock_progress_observer)
+
+    # ============= Assert ===============
+    mock_simulator.enable_logging.assert_not_called()
+    mock_simulator.enable_reporting.assert_called_once()
+    mock_simulator.set_initial_balance.assert_called_once_with(0.0)
+    mock_simulator.load_strategy.assert_called_once_with({})
+
+    assert type(result) is dict
+    assert 'status_code' not in result.keys()
+    assert result['symbol'] == 'AAPL'
+
 # FIXME: need tests for run_folder_simulation() function

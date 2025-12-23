72cebbc552360e9ab38b1c9e151e56d2f158c42b
Jason O'Connell <jaywoc12@gmail.com>
2025-12-23 17:10:11 -0500

    implemented PLPC for multi results table

--- src/StockBench/gui/results/multi/components/multi_sidebar_results_table.py
+++ src/StockBench/gui/results/multi/components/multi_sidebar_results_table.py
@@ -7,42 +7,55 @@ class MultiResultsSidebarTable(SidebarResultsTable):
 
     def __init__(self):
         super().__init__()
-        # trades made label and data label
         row = 1
         self.layout.addWidget(self.trades_made_label, row, 1)
         self.layout.addWidget(self.trades_made_data_label, row, 2)
 
-        # average trade duration label and data label
         row += 1
         self.layout.addWidget(self.average_trade_duration_label, row, 1)
         self.layout.addWidget(self.average_trade_duration_data_label, row, 2)
 
-        # effectiveness label and data label
         row += 1
         self.layout.addWidget(self.effectiveness_label, row, 1)
         self.layout.addWidget(self.effectiveness_data_label, row, 2)
 
-        # total P/L label and data label
+        row += 1
+        self.layout.addWidget(self.spacer, row, 1)
+
         row += 1
         self.layout.addWidget(self.total_pl_label, row, 1)
         self.layout.addWidget(self.total_pl_data_label, row, 2)
 
-        # average P/L label and data label
         row += 1
         self.layout.addWidget(self.average_pl_label, row, 1)
         self.layout.addWidget(self.average_pl_data_label, row, 2)
 
-        # median label and data label
         row += 1
         self.layout.addWidget(self.median_pl_label, row, 1)
         self.layout.addWidget(self.median_pl_data_label, row, 2)
 
-        # stddev label and data label
         row += 1
         self.layout.addWidget(self.stddev_pl_label, row, 1)
         self.layout.addWidget(self.stddev_pl_data_label, row, 2)
 
-        # apply the layout to the frame
+        row += 1
+        self.layout.addWidget(self.spacer, row, 1)
+
+        row += 1
+        self.layout.addWidget(self.average_plpc_label, row, 1)
+        self.layout.addWidget(self.average_plpc_data_label, row, 2)
+
+        row += 1
+        self.layout.addWidget(self.median_plpc_label, row, 1)
+        self.layout.addWidget(self.median_plpc_data_label, row, 2)
+
+        row += 1
+        self.layout.addWidget(self.stddev_plpc_label, row, 1)
+        self.layout.addWidget(self.stddev_plpc_data_label, row, 2)
+
+        row += 1
+        self.layout.addWidget(self.spacer, row, 1)
+
         self.setLayout(self.layout)
 
     def render_data(self, simulation_results: dict):
@@ -54,3 +67,6 @@ class MultiResultsSidebarTable(SidebarResultsTable):
             self.average_pl_data_label.setText(f'$ {simulation_results[AVERAGE_PL_KEY]:,.2f}')
             self.median_pl_data_label.setText(f'$ {simulation_results[MEDIAN_PL_KEY]:,.2f}')
             self.stddev_pl_data_label.setText(f'$ {simulation_results[STANDARD_DEVIATION_PL_KEY]:,.2f}')
+            self.average_plpc_data_label.setText(f'{simulation_results[AVERAGE_PLPC_KEY]:,.2f} %')
+            self.median_plpc_data_label.setText(f'{simulation_results[MEDIAN_PLPC_KEY]:,.2f} %')
+            self.stddev_plpc_data_label.setText(f'{simulation_results[STANDARD_DEVIATION_PLPC_KEY]:,.2f} %')

c9151edba4d86bebcb194ef2066b19c3ae7a48b6
Jason O'Connell <jaywoc12@gmail.com>
2025-12-23 21:12:10 -0500

    removed comments

--- src/StockBench/gui/results/multi/components/multi_sidebar_metadata_table.py
+++ src/StockBench/gui/results/multi/components/multi_sidebar_metadata_table.py
@@ -7,7 +7,6 @@ class MultiMetadataSidebarTable(SidebarResultsTable):
     """Table of overview metadata."""
     def __init__(self):
         super().__init__()
-        # strategy label and data label
         row = 1
         self.layout.addWidget(self.strategy_label, row, 1)
         # strategy data label
@@ -15,17 +14,14 @@ class MultiMetadataSidebarTable(SidebarResultsTable):
         self.strategy_data_label.setStyleSheet(self.RESULT_VALUE_STYLESHEET)
         self.layout.addWidget(self.strategy_data_label, row, 2)
 
-        # trade-able days label and data label
         row += 1
         self.layout.addWidget(self.trade_able_days_label, row, 1)
         self.layout.addWidget(self.trade_able_days_data_label, row, 2)
 
-        # elapsed time label and data label
         row += 1
         self.layout.addWidget(self.elapsed_time_label, row, 1)
         self.layout.addWidget(self.elapsed_time_data_label, row, 2)
 
-        # apply the layout to the frame
         self.setLayout(self.layout)
 
     def render_data(self, simulation_results: dict):

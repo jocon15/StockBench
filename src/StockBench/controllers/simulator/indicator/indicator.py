1d544d5d610113219544beb1fa1ee211be2f9027
dev sys <jaywoc12@gmail.com>
2026-08-13 21:05:50 -0400

    Added docstrings for indicator interface

--- src/StockBench/controllers/simulator/indicator/indicator.py
+++ src/StockBench/controllers/simulator/indicator/indicator.py
@@ -1,23 +1,33 @@
 from abc import abstractmethod
 
+from StockBench.controllers.simulator.indicator.subplot import Subplot
+
 
 class IndicatorInterface:
     @property
     @abstractmethod
-    def strategy_name(self):
+    def strategy_name(self) -> str:
+        # FIXME: validate docstring accuracy
+        """Defines the exact name the indicator appears as in a strategy file."""
         raise NotImplementedError('Not implemented yet!')
 
     @property
     @abstractmethod
-    def data_name(self):
+    def dataframe_name(self) -> str:
+        # FIXME: validate docstring accuracy
+        """Defines the exact name the indicator uses in the dataframe."""
         raise NotImplementedError('Not implemented yet!')
 
     @property
     @abstractmethod
-    def trigger(self):
+    def trigger(self) -> Trigger:
+        # FIXME: validate docstring accuracy
+        """The indicator's trigger."""
         raise NotImplementedError('Not implemented yet!')
 
     @property
     @abstractmethod
-    def subplot(self):
+    def subplot(self) -> Subplot:
+        # FIXME: validate docstring accuracy
+        """The indicator's subplot."""
         raise NotImplementedError('Not implemented yet!')

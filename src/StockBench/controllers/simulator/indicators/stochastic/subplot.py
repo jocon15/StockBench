8394cc156ee1c5362be92ffc09ad35a2be384ee6
dev sys <jaywoc12@gmail.com>
2026-08-16 23:37:03 -0400

    Added support for multiple stochastic traces on a single subplot

--- src/StockBench/controllers/simulator/indicators/stochastic/subplot.py
+++ src/StockBench/controllers/simulator/indicators/stochastic/subplot.py
@@ -13,6 +13,10 @@ class StochasticSubplot(SubplotInterface):
     A Stochastic object contains the subplot with main stochastic oscillator data. If multiple lengths of the indicator
     are used, they will be added as traces.
 
+    Primary subplot traces include:
+        - Default length stochastic (14)
+        - Custom length stochastic
+
     Additional traces include:
         - RSI upper algorithm
         - RSI lower algorithm
@@ -21,18 +25,17 @@ class StochasticSubplot(SubplotInterface):
     def __init__(self):
         super().__init__('stochastic', [{"type": "scatter"}], False)
 
+        self.remaining_traces = []
+
     def get_subplot(self, df: DataFrame) -> Scatter:
         """Builds the subplot.
 
-        This subplot could contain any number custom length (primary) stochastic oscillator traces.
-        EX: 20stochastic, 30stochastic, stochastic, etc.
-
-        They will all end up on the same subplot because they share the same scale.
+        Note: All primary subplot traces end up on the same subplot because they share the same scale.
         """
         primary_traces = []
         primary_trace_names = []
         for (column_name, column_data) in df.items():
-            if {self.data_symbol} in column_name and column_name not in primary_trace_names:
+            if self.data_symbol in column_name and column_name not in primary_trace_names:
                 primary_traces.append(fplt.Scatter(
                     x=df['Date'],
                     y=column_data,
@@ -43,23 +46,22 @@ class StochasticSubplot(SubplotInterface):
             raise SimulationDataException('Stochastic subplot build invoked but no data element labeled stochastic was '
                                           'encountered!')
 
-        subplot = primary_traces[0]
-        for trace in primary_traces:
-            subplot.add_trace(trace)
+        # store remaining traces
+        self.remaining_traces = primary_traces.copy()
+        self.remaining_traces.pop(0)
 
-        return subplot
+        return primary_traces[0]
 
     def get_traces(self, df: DataFrame) -> list:
         """Builds and a list of traces to add to the subplot."""
         # builds and returns a list of traces to add to the subplot
-        traces = []
         for (column_name, column_data) in df.items():
             # stochastic + underscore indicates it is a stochastic trigger value
             if f'{self.data_symbol}_' in column_name:
-                traces.append(fplt.Scatter(
+                self.remaining_traces.append(fplt.Scatter(
                     x=df['Date'],
                     y=df[column_name],
                     line=dict(color=HORIZONTAL_TRIGGER_YELLOW),
                     name=column_name))
 
-        return traces
+        return self.remaining_traces
